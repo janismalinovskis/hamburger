9edf59451e797fc7057b512783ef5455c1a588b6
janismalinovskis <77164437+janismalinovskis@users.noreply.github.com>
2021-05-28 15:19:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
 function responsive() {
-  let navigacija = document.getElementById("navigacija");
-  if (navigacija.className === "navlinks") {
-    navigacija.className = navigacija.className + "small-screen";
-  } else {
-    navigacija.className = "navlinks";
+    let navigacija = document.getElementById("navigacija");
+    if (navigacija.className === "navlinks") {
+      navigacija.className += " small-screen";
+    } else {
+      navigacija.className = "navlinks";
+    }
   }
-}
+  
